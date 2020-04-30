df0d0ed2c50ac4d3a9a3af5cdbadc5ed671d2d01
nls-jajuko <nls-jajko@users.noreply.github.com>
2020-04-30 23:51:24 +0300

    quick und dirty geocoding tab for osku

--- geocoding-oskari-poc.js
+++ geocoding-oskari-poc.js
@@ -1,116 +1,107 @@
 /* Quick Geocoding Tab for Oskari */
-Oskari.clazz.define('Oskari.mapframework.bundle.search.GeocodingView',
-    function (instance) {
-        this.instance = instance;
-        this.sandbox = this.instance.getSandbox();
-        this.searchservice = instance.service;
-        this.state = null;
-        this.lastResult = null;
-        this.lastSort = null;
-        this.resultActions = {};
-        this._searchContainer = null;
+Oskari.clazz.define(
+    'Oskari.mapframework.bundle.search.GeocodingView',
+    instance => {
+        Object.assign(this, {
+            instance: instance,
+            sandbox: instance.getSandbox(),
+            searchservice: instance.service,
+            state: null,
+            lastResult: null,
+            lastSort: null,
+            resultActions: {},
+            _searchContainer: null,
+            resultHeaders: [
+                {
+                    title: instance.getLocalization('grid').name,
+                    prop: 'name'
+                }, {
+                    title: instance.getLocalization('grid').region,
+                    prop: 'region'
+                }, {
+                    title: instance.getLocalization('grid').type,
+                    prop: 'type'
+                }
+            ]
+        });
+    }, {
+    __doSearch: function () {
+        let me = this,
+            field = this.getField(),
+            button = this.getButton(),
+            searchContainer = this.getContainer(),
+            searchString = field.getValue(this.instance.safeChars);
 
-        this.geocoding_controller = new AbortController(),
-            this.geocoding_signal = this.geocoding_controller.signal;
+        searchContainer.find('div.resultList').empty();
+        searchContainer.find('div.info').empty();
 
-        this.resultHeaders = [
-            {
-                title: this.instance.getLocalization('grid').name,
-                prop: 'name'
-            }, {
-                title: this.instance.getLocalization('grid').region,
-                prop: 'region'
-            }, {
-                title: this.instance.getLocalization('grid').type,
-                prop: 'type'
-            }
-        ];
+        if (!searchString || searchString.length == 0) {
+            field.setEnabled(true);
+            button.setEnabled(true);
+            return;
+        }
 
+        let self = this,
+            lang = Oskari.getLang(),
+            sb = this.sandbox || Oskari.getSandbox(),
+            epsg = sb.findRegisteredModuleInstance('MainMapModule').getProjection(),
+            epsgCode = epsg.split(':')[1];
+
+        let url = 'https://avoin-paikkatieto.maanmittauslaitos.fi'
+            + '/geocoding/v1/pelias/search?'
+            + '&lang=' + lang
+            + '&api-key=7cd2ddae-9f2e-481c-99d0-404e7bc7a0b2'
+            + '&crs=http://www.opengis.net/def/crs/EPSG/0/' + epsgCode
+            + '&size=50'
+            + '&text=' + searchString;
+
+        if (this.geocoding_controller) {
+            this.geocoding_controller.abort();
+        }
+        this.geocoding_controller = new AbortController();
+        this.geocoding_signal = this.geocoding_controller.signal;
+
+        fetch(url, {
+            method: 'get',
+            signal: this.geocoding_signal
+        }).then(r => r.json()).then(json => {
+            let res = {
+                "methods": [
+                    {},
+                    {},
+                    {}
+                ],
+                "locations": [],
+                "totalCount": 0
+            };
+
+            res.locations = json.features.map(f => {
+                let fprops = f.properties;
+                return {
+                    "zoomScale": 5000,
+                    "name": fprops.label,
+                    "rank": fprops.rank,
+                    "lon": f.geometry.coordinates[0],
+                    "id": fprops.placeId,
+                    "type": fprops['label:placeType'],
+                    "region": fprops['label:municipality'],
+                    "village": fprops['label'],
+                    "lat": f.geometry.coordinates[1],
+                    "channelId": "REGISTER_OF_NOMENCLATURE_CHANNEL"
+                };
+            });
+            res.totalCount = res.locations.length;
+
+            self.handleSearchResult(true, res, searchString);
+
+        }).catch(function (err) {
+        });
     },
-    {
-        __doSearch: function () {
-            var me = this,
-                field = this.getField(),
-                button = this.getButton(),
-                searchContainer = this.getContainer();
-
-            searchContainer.find('div.resultList').empty();
-            searchContainer.find('div.info').empty();
-            var searchString = field.getValue(this.instance.safeChars);
-
-            if (!searchString || searchString.length == 0) {
-                field.setEnabled(true);
-                button.setEnabled(true);
-                return;
-            }
-
-            var self = this,
-                lang = Oskari.getLang(),
-                sb = this.sandbox || Oskari.getSandbox(),
-                epsg = sb.findRegisteredModuleInstance('MainMapModule').getProjection(),
-                epsgCode = epsg.split(':')[1],
-                evtBuilder = Oskari.eventBuilder('SearchResultEvent');
-
-            var url = 'https://avoin-paikkatieto.maanmittauslaitos.fi'
-                + '/geocoding/v1/pelias/search?'
-                + '&lang=' + lang
-                + '&api-key=7cd2ddae-9f2e-481c-99d0-404e7bc7a0b2'
-                + '&crs=http://www.opengis.net/def/crs/EPSG/0/' + epsgCode
-                + '&size=50'
-                + '&text=' + searchString;
-
-            if (this.geocoding_controller) {
-                this.geocoding_controller.abort();
-                /* don't know how to reuse - aborts early if reused */
-                this.geocoding_controller = new AbortController();
-                this.geocoding_signal = this.geocoding_controller.signal;
-            }
-
-            fetch(url, {
-                method: 'get',
-                signal: this.geocoding_signal
-            }).
-                then(r => r.json()).
-                then(json => {
-                    let res = {
-                        "methods": [
-                            {},
-                            {},
-                            {}
-                        ],
-                        "locations": [],
-                        "totalCount": 0
-                    };
-
-                    res.locations = json.features.map(f => {
-                        let fprops = f.properties;
-                        return {
-                            "zoomScale": 5000,
-                            "name": fprops.label,
-                            "rank": fprops.rank,
-                            "lon": f.geometry.coordinates[0],
-                            "id": fprops.placeId,
-                            "type": fprops['label:placeType'],
-                            "region": fprops['label:municipality'],
-                            "village": fprops['label'],
-                            "lat": f.geometry.coordinates[1],
-                            "channelId": "REGISTER_OF_NOMENCLATURE_CHANNEL"
-                        };
-                    });
-                    res.totalCount = res.locations.length;
-                    self.handleSearchResult(true, res, searchString);
-                }).catch(function (err) {
-                    //geocoding_reset_list(listEl);
-                });
-        },
-        __doAutocompleteSearch: function () {
-            this.__doSearch();
-
-        },
-        _validateSearchKey: function (key) { return true; }
-
-
+    __doAutocompleteSearch: function () {
+        this.__doSearch();
     },
+    _validateSearchKey: function (key) { return true; }
+},
     {
         extend: ['Oskari.mapframework.bundle.search.DefaultView']
     }
@@ -118,7 +109,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.search.GeocodingView',
 
 /** hack */
 function add() {
-    var ui = jQuery('<div />'),
+    let ui = jQuery('<div />'),
         sandbox = Oskari.getSandbox(),
         lang = Oskari.getLang(),
         search = sandbox.findRegisteredModuleInstance('Search'),
@@ -130,12 +121,12 @@ function add() {
 
     search.conf.autocomplete = true;
 
-    var view = Oskari.clazz.create('Oskari.mapframework.bundle.search.GeocodingView', search);
+    let view = Oskari.clazz.create('Oskari.mapframework.bundle.search.GeocodingView', search);
     view.createUi(ui);
-    var priority = this.tabPriority,
+
+    let priority = 2,//this.tabPriority,
         id = 'oskari_metadatacatalogue_tabpanel_header',
-        reqBuilder = Oskari.requestBuilder('Search.AddTabRequest'),
-        req = reqBuilder(title, ui, priority, id);
+        req = Oskari.requestBuilder('Search.AddTabRequest')(title, ui, priority, id);
 
     sandbox.request(search, req);
 }
