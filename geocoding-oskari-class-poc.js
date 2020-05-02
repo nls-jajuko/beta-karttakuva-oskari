8844d00bc7073bf49a99cadf4da61ca695c3f53c
nls-jajuko <nls-jajko@users.noreply.github.com>
2020-05-02 19:18:07 +0300

    more modern impl

--- geocoding-oskari-class-poc.js
+++ geocoding-oskari-class-poc.js
@@ -1,12 +1,80 @@
-/* Quick Geocoding Tab for Oskari */
+/* Geocoding Service for Oskari */
+class GeocodingService {
+    constructor(urls, epsgCode, lang) {
+        this.urls = urls;
+        this.queryParams = {
+            'api-key': '7cd2ddae-9f2e-481c-99d0-404e7bc7a0b2',
+            'sources': 'addresses,geographic-names',
+            'crs': 'http://www.opengis.net/def/crs/EPSG/0/' + epsgCode,
+            'lang': lang
+        }
+    }
+
+    search(searchString) {
+        let url =
+            this.geocodingURL('search', {
+                'size': '50',
+                'text': searchString
+            });
+
+        if (this.geocoding_controller) {
+            this.geocoding_controller.abort();
+        }
+        this.geocoding_controller = new AbortController();
+        this.geocoding_signal = this.geocoding_controller.signal;
+
+        return fetch(url, {
+            method: 'get',
+            signal: this.geocoding_signal
+        });
+    }
+
+    similar(searchString) {
+        let url = this.geocodingURL('similar', {
+            'size': '10',
+            'text': searchString
+        });
+
+        if (this.similar_controller) {
+            this.similar_controller.abort();
+        }
+        this.similar_controller = new AbortController();
+        this.similar_signal = this.similar_controller.signal;
+
+        return fetch(url, {
+            method: 'get',
+            signal: this.similar_signal
+        });
+
+    }
+
+    geocodingURL(urlkey, args) {
+        let search = new URLSearchParams(),
+            params = this.queryParams,
+            combined = { ...params, ...args };
+        for (let [key, value] of Object.entries(combined)) {
+            search.append(key, value);
+        }
+        return this.urls[urlkey] + search.toString();
+    }
+
+}
+/* Geocoding Tab for Oskari */
 class GeocodingView extends Oskari.clazz.get('Oskari.mapframework.bundle.search.DefaultView') {
+    constructor(instance) {
+        super(instance);
+        let epsg = this.sandbox.findRegisteredModuleInstance('MainMapModule').getProjection(),
+            epsgCode = epsg.split(':')[1],
+            lang = Oskari.getLang(),
+            urls = {
+                search: 'https://avoin-paikkatieto.maanmittauslaitos.fi/geocoding/v1/pelias/search?',
+                similar: 'https://avoin-paikkatieto.maanmittauslaitos.fi/geocoding/v1/searchterm/similar?'
+            };
+        this.service = new GeocodingService(urls, epsgCode, lang);
+    }
 
     __doSearch() {
         let self = this,
-            lang = Oskari.getLang(),
-            sandbox = this.sandbox,
-            epsg = sandbox.findRegisteredModuleInstance('MainMapModule').getProjection(),
-            epsgCode = epsg.split(':')[1],
             field = this.getField(),
             searchContainer = this.getContainer(),
             searchString = field.getValue(this.instance.safeChars),
@@ -22,37 +90,18 @@ class GeocodingView extends Oskari.clazz.get('Oskari.mapframework.bundle.search.
             return;
         }
 
-        let url = 'https://avoin-paikkatieto.maanmittauslaitos.fi'
-            + '/geocoding/v1/pelias/search?'
-            + '&sources=addresses,geographic-names'
-            + '&lang=' + lang
-            + '&api-key=7cd2ddae-9f2e-481c-99d0-404e7bc7a0b2'
-            + '&crs=http://www.opengis.net/def/crs/EPSG/0/' + epsgCode
-            + '&size=50'
-            + '&text=' + searchString;
-
-        if (this.geocoding_controller) {
-            this.geocoding_controller.abort();
-        }
-        this.geocoding_controller = new AbortController();
-        this.geocoding_signal = this.geocoding_controller.signal;
-
-        fetch(url, {
-            method: 'get',
-            signal: this.geocoding_signal
-        }).then(r => r.json()).then(json => {
+        this.service.search(searchString).then(r => r.json()).then(json => {
             let res = {
                 locations: json.features.map(f => {
-                    let fprops = f.properties;
                     return {
                         "zoomScale": 5000,
-                        "name": fprops.label,
-                        "rank": fprops.rank,
+                        "name": f.properties.label,
+                        "rank": f.properties.rank,
                         "lon": f.geometry.coordinates[0],
-                        "id": fprops.placeId,
-                        "type": fprops['label:placeType'],
-                        "region": fprops['label:municipality'],
-                        "village": fprops['label'],
+                        "id": f.properties.placeId,
+                        "type": f.properties['label:placeType'],
+                        "region": f.properties['label:municipality'],
+                        "village": f.properties['label'],
                         "lat": f.geometry.coordinates[1],
                         "channelId": "GEOCODING"
                     }
@@ -61,8 +110,7 @@ class GeocodingView extends Oskari.clazz.get('Oskari.mapframework.bundle.search.
             res.totalCount = res.locations.length;
             self.handleSearchResult(true, res, searchString);
 
-        }).catch(function (err) {
-        });
+        }).catch(function (err) { /* we fail often due to autocomplete*/ });
     }
 
     __doAutocompleteSearch() {
@@ -76,23 +124,7 @@ class GeocodingView extends Oskari.clazz.get('Oskari.mapframework.bundle.search.
 
         this.__doSearch();
 
-        let url = 'https://avoin-paikkatieto.maanmittauslaitos.fi'
-            + '/geocoding/v1/searchterm/similar?'
-            + '&lang=' + lang
-            + '&api-key=7cd2ddae-9f2e-481c-99d0-404e7bc7a0b2'
-            + '&size=10'
-            + '&text=' + searchString;
-
-        if (this.similar_controller) {
-            this.similar_controller.abort();
-        }
-        this.similar_controller = new AbortController();
-        this.similar_signal = this.similar_controller.signal;
-
-        fetch(url, {
-            method: 'get',
-            signal: this.similar_signal
-        }).then(r => r.json()).then(json => {
+        this.service.similar(searchString).then(r => r.json()).then(json => {
             if (!json.terms || !json.terms.length) {
                 return;
             }
@@ -103,10 +135,12 @@ class GeocodingView extends Oskari.clazz.get('Oskari.mapframework.bundle.search.
                 };
             });
 
-            field.autocomplete(autocompleteValues);
-        }).catch(function (err) {
-        })
+            if (autocompleteValues.length) {
+                field.autocomplete(autocompleteValues);
+            }
+        }).catch(function (err) { /* we fail often due to autocomplete*/ });
     }
+
     _validateSearchKey(key) {
         return true;
     }
