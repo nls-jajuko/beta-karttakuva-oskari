ec66b2e996cb18762ee6c4a5798560c2c5eb0639
nls-jajuko <nls-jajko@users.noreply.github.com>
2020-05-02 22:05:43 +0300

    similar terms

--- GeocodingView.js
+++ GeocodingView.js
@@ -2,7 +2,8 @@ import GeocodingService from './GeocodingService';
 
 const SearchDefaultView = Oskari.clazz.get('Oskari.mapframework.bundle.search.DefaultView');
 
-export class GeocodingView extends SearchDefaultView {
+export /* Geocoding Tab for Oskari */
+class GeocodingView extends SearchDefaultView {
     constructor(instance) {
         super(instance);
         let epsg = this.sandbox.findRegisteredModuleInstance('MainMapModule').getProjection(),
@@ -13,24 +14,47 @@ export class GeocodingView extends SearchDefaultView {
                 similar: 'https://avoin-paikkatieto.maanmittauslaitos.fi/geocoding/v1/searchterm/similar?'
             };
         this.service = new GeocodingService(urls, epsgCode, lang);
+        this.lastSearchString = undefined;
+    }
+
+    createUi(tab) {
+        super.createUi(tab);
+        let searchContainerEl = this.getContainer()[0],
+            controlsEl = searchContainerEl.querySelector('div.controls'),
+            similarEl = document.createElement('div');
+        controlsEl.appendChild(similarEl)
+        this.similarEl = similarEl;
+
     }
 
     __doSearch() {
         let self = this,
             field = this.getField(),
+            button = this.getButton(),
             searchContainer = this.getContainer(),
             searchString = field.getValue(this.instance.safeChars),
             resultsEl = searchContainer[0].querySelectorAll('div.resultList'),
             infoEl = searchContainer[0].querySelectorAll('div.info');
 
+
+
+        if (!searchString || searchString.length == 0) {
+            self.progressSpinner.stop();
+            field.setEnabled(true);
+            button.setEnabled(true);
+            return;
+        }
+        if (this.lastSearchString === searchString) {
+            self.progressSpinner.stop();
+            field.setEnabled(true);
+            button.setEnabled(true);
+            return;
+        }
+
         while (resultsEl.firstChild)
             resultsEl.removeChild(resultsEl.firstChild);
         while (infoEl.firstChild)
             infoEl.removeChild(infoEl.firstChild);
-
-        if (!searchString || searchString.length == 0) {
-            return;
-        }
 
         this.service.search(searchString).then(r => r.json()).then(json => {
             let res = {
@@ -50,17 +74,26 @@ export class GeocodingView extends SearchDefaultView {
                 })
             };
             res.totalCount = res.locations.length;
+            if (res.totalCount == 0) {
+                res.totalCount = -2;
+            }
+            self.lastSearchString = searchString;
             self.handleSearchResult(true, res, searchString);
 
-        }).catch(function (err) { /* we fail often due to autocomplete*/ });
+        }).catch(function (err) {
+            /* we fail often due to autocomplete*/
+        });
     }
 
     __doAutocompleteSearch() {
-        let lang = Oskari.getLang(),
-            field = this.getField(),
+        let self = this,
+            field = self.getField(),
             searchString = field.getValue(this.instance.safeChars);
 
         if (!searchString || searchString.length == 0) {
+            return;
+        }
+        if (this.lastSimilarString === searchString) {
             return;
         }
 
@@ -76,14 +109,48 @@ export class GeocodingView extends SearchDefaultView {
                     value: f.text.indexOf(' ') != -1 ? '"' + f.text + '"' : f.text, data: f.text
                 };
             });
+            this.lastSimilarString = searchString;
 
-            if (autocompleteValues.length) {
-                field.autocomplete(autocompleteValues);
-            }
-        }).catch(function (err) { /* we fail often due to autocomplete*/ });
+            let similarEl = this.similarEl;
+            while (similarEl.firstChild)
+                similarEl.removeChild(similarEl.firstChild);
+
+            autocompleteValues.forEach(e => {
+                let spacing = document.createTextNode(' '),
+                    el = document.createElement('a');
+                el.setAttribute('href', 'javascript:void(0)');
+                el.innerHTML = e.value;
+                el.dataset.value = e.value;
+                el.addEventListener("click", ev => {
+                    field.setValue(ev.target.dataset.value);
+                    self.__doAutocompleteSearch();
+                });
+                similarEl.appendChild(el);
+                similarEl.appendChild(spacing);
+            });
+
+        }).catch(function (err) {
+        });
     }
 
     _validateSearchKey(key) {
         return true;
+    }
+
+    __getSearchResultHeader(count, hasMore) {
+        if (count < 0) {
+            return "";
+        }
+        var intro = _.template(this.instance.getLocalization('searchResultCount') + ' ${count} ' +
+            this.instance.getLocalization('searchResultCount2'));
+        var msg = intro({ count: count });
+        msg = msg + '<br/>';
+
+        if (hasMore) {
+            // more results available
+            msg = msg + this.instance.getLocalization('searchResultDescriptionMoreResults');
+            msg = msg + '<br/>';
+        }
+        return msg + this.instance.getLocalization('searchResultDescriptionOrdering');
     }
 }
--- geocoding-oskari-class-poc.js
+++ geocoding-oskari-class-poc.js
@@ -8,6 +8,7 @@ class GeocodingService {
             'crs': 'http://www.opengis.net/def/crs/EPSG/0/' + epsgCode,
             'lang': lang
         }
+
     }
 
     search(searchString) {
@@ -71,24 +72,47 @@ class GeocodingView extends Oskari.clazz.get('Oskari.mapframework.bundle.search.
                 similar: 'https://avoin-paikkatieto.maanmittauslaitos.fi/geocoding/v1/searchterm/similar?'
             };
         this.service = new GeocodingService(urls, epsgCode, lang);
+        this.lastSearchString = undefined;
+    }
+
+    createUi(tab) {
+        super.createUi(tab);
+        let searchContainerEl = this.getContainer()[0],
+            controlsEl = searchContainerEl.querySelector('div.controls'),
+            similarEl = document.createElement('div');
+        controlsEl.appendChild(similarEl)
+        this.similarEl = similarEl;
+
     }
 
     __doSearch() {
         let self = this,
             field = this.getField(),
+            button = this.getButton(),
             searchContainer = this.getContainer(),
             searchString = field.getValue(this.instance.safeChars),
             resultsEl = searchContainer[0].querySelectorAll('div.resultList'),
             infoEl = searchContainer[0].querySelectorAll('div.info');
 
+
+
+        if (!searchString || searchString.length == 0) {
+            self.progressSpinner.stop();
+            field.setEnabled(true);
+            button.setEnabled(true);
+            return;
+        }
+        if (this.lastSearchString === searchString) {
+            self.progressSpinner.stop();
+            field.setEnabled(true);
+            button.setEnabled(true);
+            return;
+        }
+
         while (resultsEl.firstChild)
             resultsEl.removeChild(resultsEl.firstChild);
         while (infoEl.firstChild)
             infoEl.removeChild(infoEl.firstChild);
-
-        if (!searchString || searchString.length == 0) {
-            return;
-        }
 
         this.service.search(searchString).then(r => r.json()).then(json => {
             let res = {
@@ -108,17 +132,26 @@ class GeocodingView extends Oskari.clazz.get('Oskari.mapframework.bundle.search.
                 })
             };
             res.totalCount = res.locations.length;
+            if (res.totalCount == 0) {
+                res.totalCount = -2;
+            }
+            self.lastSearchString = searchString;
             self.handleSearchResult(true, res, searchString);
 
-        }).catch(function (err) { /* we fail often due to autocomplete*/ });
+        }).catch(function (err) {
+            /* we fail often due to autocomplete*/
+        });
     }
 
     __doAutocompleteSearch() {
-        let lang = Oskari.getLang(),
-            field = this.getField(),
+        let self = this,
+            field = self.getField(),
             searchString = field.getValue(this.instance.safeChars);
 
         if (!searchString || searchString.length == 0) {
+            return;
+        }
+        if (this.lastSimilarString === searchString) {
             return;
         }
 
@@ -134,15 +167,49 @@ class GeocodingView extends Oskari.clazz.get('Oskari.mapframework.bundle.search.
                     value: f.text.indexOf(' ') != -1 ? '"' + f.text + '"' : f.text, data: f.text
                 };
             });
+            this.lastSimilarString = searchString;
 
-            if (autocompleteValues.length) {
-                field.autocomplete(autocompleteValues);
-            }
-        }).catch(function (err) { /* we fail often due to autocomplete*/ });
+            let similarEl = this.similarEl;
+            while (similarEl.firstChild)
+                similarEl.removeChild(similarEl.firstChild);
+
+            autocompleteValues.forEach(e => {
+                let spacing = document.createTextNode(' '),
+                    el = document.createElement('a');
+                el.setAttribute('href', 'javascript:void(0)');
+                el.innerHTML = e.value;
+                el.dataset.value = e.value;
+                el.addEventListener("click", ev => {
+                    field.setValue(ev.target.dataset.value);
+                    self.__doAutocompleteSearch();
+                });
+                similarEl.appendChild(el);
+                similarEl.appendChild(spacing);
+            });
+
+        }).catch(function (err) {
+        });
     }
 
     _validateSearchKey(key) {
         return true;
+    }
+
+    __getSearchResultHeader(count, hasMore) {
+        if (count < 0) {
+            return "";
+        }
+        var intro = _.template(this.instance.getLocalization('searchResultCount') + ' ${count} ' +
+            this.instance.getLocalization('searchResultCount2'));
+        var msg = intro({ count: count });
+        msg = msg + '<br/>';
+
+        if (hasMore) {
+            // more results available
+            msg = msg + this.instance.getLocalization('searchResultDescriptionMoreResults');
+            msg = msg + '<br/>';
+        }
+        return msg + this.instance.getLocalization('searchResultDescriptionOrdering');
     }
 }
 
